a970033d61a03ecb420f4e271b03495f99432aca
kdhola <ks328392@dal.ca>
2023-11-30 10:40:13 -0400

    added code with magic number 4

--- Magic_smell2.py
+++ Magic_smell2.py
@@ -1,22 +1,19 @@
 import random
 
 def discount(price):
-    # Magic number not clear
     magic_number = random.uniform(0.1, 0.3)
     discounted_price = price - (price * magic_number)
     return discounted_price
 
 def main():
-    # Single-letter variable names
+
     p = float(input("Enter the original price: "))
-    
-    # Inconsistent variable naming
+
     mn = 0.2
 
     dp = discount(p)
 
     print(f"Original Price: ${p:.2f}")
-    # Using f-string unnecessarily
     print("Discounted Price: $" + str(round(dp, 2)))
 
 if __name__ == "__main__":
